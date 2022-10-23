bec4a428826e686ba0fd620e2a709caf1d1a400e
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-10-24 00:25:33 +0200

    changed to connection pooling

---
+++ src/db/pool.ts
@@ -0,0 +1,16 @@
+import { Pool } from 'pg'
+
+// pools will use environment variables for connection information
+const pool = new Pool()
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+
+// you can also use async/await
+pool.query('SELECT NOW()').then((res) => {
+  console.log(res.rows[0])
+})
+
+export default pool
