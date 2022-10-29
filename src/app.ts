1c5b0cf04bf3c5530b00c27ab4e664cbc5465916
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-10-29 23:08:19 +0200

    added pipeline

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,36 @@ import { ScrapingURLs } from './shared'
 import { extractTeam } from './modules/extractTeam'
 import { extractParticipations } from './modules/extractParticipations'
 
+import { JSDOM } from 'jsdom'
+
+import fs from 'fs'
+import { doc } from 'prettier'
+const fsPromises = fs.promises
+
+async function doReadFile() {
+  try {
+    // Using the filehandle method
+    const filehandle = await fsPromises.open(
+      `${__dirname}/scraper/testFiles/Player.html`,
+      'r+'
+    )
+    const data = await filehandle.readFile('utf8')
+    filehandle.close()
+    console.log(data.length)
+
+    const dom = new JSDOM(data)
+    const document = dom.window.document
+    console.log(document.querySelectorAll('table').length)
+
+    const player = extractPlayer(document, 2)
+    console.log(player)
+  } catch (e) {
+    console.log('Error', e)
+  }
+}
+
+doReadFile()
+
 const hostname = process.env.HOSTNAME
 const port = process.env.PORT
 
@@ -17,23 +47,32 @@ setupExpressServer().then((server) => {
 })
 
 // player
-const playerID = 60060
-const playerURL = ScrapingURLs.Player(playerID)
-fetchHTML(playerURL).then((document) => {
-  const player = extractPlayer(document, playerID)
-  console.log(player)
-  DB.insert.player(player)
-})
+// const playerID = 60060
+// const playerURL = ScrapingURLs.Player(playerID)
+// fetchHTML(playerURL).then((document) => {
+//   const player = extractPlayer(document, playerID)
+//   console.log(player)
+//   DB.insert.player(player)
+// })
+
+// for (let i = 0; i <= 1; i++) {
+//   const playerURL = ScrapingURLs.Player(i)
+//   fetchHTML(playerURL).then((document) => {
+//     const player = extractPlayer(document, i)
+//     console.log(player)
+//     DB.insert.player(player)
+//   })
+// }
 
 // team & participations
-const teamID = 51076
-const teamURL = ScrapingURLs.Team(teamID)
-fetchHTML(teamURL).then((document) => {
-  const team = extractTeam(document, teamID)
-  console.log(team)
-  DB.insert.team(team)
+// const teamID = 51076
+// const teamURL = ScrapingURLs.Team(teamID)
+// fetchHTML(teamURL).then((document) => {
+//   const team = extractTeam(document, teamID)
+//   console.log(team)
+//   DB.insert.team(team)
 
-  const participations = extractParticipations(document, teamID)
-  console.log(participations)
-  DB.insert.participations(participations)
-})
+//   const participations = extractParticipations(document, teamID)
+//   console.log(participations)
+//   DB.insert.participations(participations)
+// })
