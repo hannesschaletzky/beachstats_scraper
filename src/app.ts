9478b76fa64f738456462cce50b37bdff908db94
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-11-14 14:30:08 +0100

    working app

--- src/app.ts
+++ src/app.ts
@@ -13,29 +13,29 @@ import fs from 'fs'
 import { doc } from 'prettier'
 const fsPromises = fs.promises
 
-async function doReadFile() {
-  try {
-    // Using the filehandle method
-    const filehandle = await fsPromises.open(
-      `${__dirname}/scraper/testFiles/Player.html`,
-      'r+'
-    )
-    const data = await filehandle.readFile('utf8')
-    filehandle.close()
-    console.log(data.length)
+// async function doReadFile() {
+//   try {
+//     // Using the filehandle method
+//     const filehandle = await fsPromises.open(
+//       `${__dirname}/scraper/testFiles/Player.html`,
+//       'r+'
+//     )
+//     const data = await filehandle.readFile('utf8')
+//     filehandle.close()
+//     console.log(data.length)
 
-    const dom = new JSDOM(data)
-    const document = dom.window.document
-    console.log(document.querySelectorAll('table').length)
+//     const dom = new JSDOM(data)
+//     const document = dom.window.document
+//     console.log(document.querySelectorAll('table').length)
 
-    const player = extractPlayer(document, 2)
-    console.log(player)
-  } catch (e) {
-    console.log('Error', e)
-  }
-}
+//     const player = extractPlayer(document, 2)
+//     console.log(player)
+//   } catch (e) {
+//     console.log('Error', e)
+//   }
+// }
 
-doReadFile()
+// doReadFile()
 
 const hostname = process.env.HOSTNAME
 const port = process.env.PORT
