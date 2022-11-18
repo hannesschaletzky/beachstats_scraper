58bcf9992b0be71fad692b5db9d13ffb0169f0fb
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-11-18 18:49:31 +0100

    changed tsc target to es6

--- src/scraper/crawlee.ts
+++ src/scraper/crawlee.ts
@@ -1,22 +1,22 @@
-// https://github.com/apify/crawlee#manual-installation
+// // https://github.com/apify/crawlee#manual-installation
 
-import { PlaywrightCrawler, Dataset } from 'crawlee'
+// import { PlaywrightCrawler, Dataset } from 'crawlee'
 
-// PlaywrightCrawler crawls the web using a headless browser controlled by the Playwright library.
-export const crawler = new PlaywrightCrawler({
-  // Use the requestHandler to process each of the crawled pages.
-  async requestHandler({ request, page, enqueueLinks, log }) {
-    const title = await page.title()
-    log.info(`Title of ${request.loadedUrl} is '${title}'`)
+// // PlaywrightCrawler crawls the web using a headless browser controlled by the Playwright library.
+// export const crawler = new PlaywrightCrawler({
+//   // Use the requestHandler to process each of the crawled pages.
+//   async requestHandler({ request, page, enqueueLinks, log }) {
+//     const title = await page.title()
+//     log.info(`Title of ${request.loadedUrl} is '${title}'`)
 
-    // const tables = await page.
+//     // const tables = await page.
 
-    // Save results as JSON to ./storage/datasets/default
-    await Dataset.pushData({ title, url: request.loadedUrl })
+//     // Save results as JSON to ./storage/datasets/default
+//     await Dataset.pushData({ title, url: request.loadedUrl })
 
-    // Extract links from the current page and add them to the crawling queue.
-    // await enqueueLinks()
-  }
-  // Uncomment this option to see the browser window.
-  // headless: false,
-})
+//     // Extract links from the current page and add them to the crawling queue.
+//     // await enqueueLinks()
+//   }
+//   // Uncomment this option to see the browser window.
+//   // headless: false,
+// })
--- src/scraper/got.ts
+++ src/scraper/got.ts
@@ -1,16 +1,16 @@
-// https://github.com/sindresorhus/got/blob/HEAD/documentation/quick-start.md
+// // https://github.com/sindresorhus/got/blob/HEAD/documentation/quick-start.md
 
-import got from 'got'
+// import got from 'got'
 
-// const response = await got(url)
+// // const response = await got(url)
 
-export function scrapeBodyGot(url: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    got(url)
-      .then(({ body }) => resolve(body as string))
-      .catch((err) => {
-        console.log(err)
-        reject(err)
-      })
-  })
-}
+// export function scrapeBodyGot(url: string): Promise<string> {
+//   return new Promise((resolve, reject) => {
+//     got(url)
+//       .then(({ body }) => resolve(body as string))
+//       .catch((err) => {
+//         console.log(err)
+//         reject(err)
+//       })
+//   })
+// }
