05e75db324e2d084e5df54f9292a9473fc66abb1
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-12-04 19:16:19 +0100

    added build step

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,11 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import { CronController } from 'modules/cron/cronController'
 import { saveBodyAsFile } from 'scraper/FS'
-import { DvvURLs, Tables } from 'shared'
-import { DB } from 'db/queries'
+import { DvvURLs } from 'shared'
 
 console.log(`Scraper started successfully`)
 
 CronController.start()
-
-// DB.ID.missing(Tables.Players).then((val) => {
-//   console.log(val)
-// })
 
 // eslint-disable-next-line no-constant-condition
 if (false) {
